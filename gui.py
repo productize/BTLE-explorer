a51d9b54034d6e98e50c47f8b43ccb59fc49f90a
Joost Yervante Damad <joost@damad.be>
2014-02-23 19:47:29 +0100

    GUI work

--- gui.py
+++ gui.py
@@ -2,21 +2,36 @@
 #
 # (c) 2014 Joost Yervante Damad <joost@productize.be>
 
-import sys
+import sys, time
 
 from PySide import QtGui, QtCore
 from PySide.QtCore import Qt
+
+class CollectThread(QtCore.QThread):
+  def __init__(self, parent = None):
+    super(CollectThread, self).__init__(parent)
+
+  def run(self):
+    while True:
+      print "cucu"
+      time.sleep(10)
+
 
 class MainWin(QtGui.QMainWindow):
 
   def __init__(self):
     super(MainWin, self).__init__()
-
     menuBar = self.menuBar()
     fileMenu = menuBar.addMenu('&File')
     self.add_action(fileMenu, '&Quit', self.close, 'Ctrl+Q')
     helpMenu = menuBar.addMenu('&Help')
     self.add_action(helpMenu, '&About', self.about)
+    self.qtab = QtGui.QTabWidget()
+    self.qtab.addTab(self.make_collect_widget(), "collect")
+    self.setCentralWidget(self.qtab)
+    self.qtab.currentChanged.connect(self.tab_changed)
+    self.qtab.setCurrentIndex(0)
+    self.run_collection()
 
   def add_action(self, menu, text, slot, shortcut=None, checkable=False, checked=False):
     action = QtGui.QAction(text, self)
@@ -40,7 +55,24 @@ class MainWin(QtGui.QMainWindow):
     QtGui.QMessageBox.about(self, "about BTLE tool", a)
 
   def close(self):
+    # TODO stop threads
     QtGui.qApp.quit()
+
+  def make_collect_widget(self):
+    self.collect_view = QtGui.QTreeView()
+    self.collect_model = QtGui.QStandardItemModel()
+    self.collect_model.setColumnCount(4)
+    self.collect_model.setHorizontalHeaderLabels(['time','from', 'name', 'data'])
+    self.collect_model_root = self.collect_model.invisibleRootItem()
+    self.collect_view.setModel(self.collect_model)
+    return self.collect_view
+
+  def tab_changed(self, i):
+    print "tab changed", i
+
+  def run_collection(self):
+    self.collect_thread = CollectThread()
+    self.collect_thread.start()
 
 def main():
   QtCore.QCoreApplication.setOrganizationName("productize")

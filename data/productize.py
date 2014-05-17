86287330ffa5a0b6a943221e5d77ccb4063842ee
Joost Yervante Damad <joost@damad.be>
2014-05-17 11:45:25 +0200

    lux51 support

--- data/productize.py
+++ data/productize.py
@@ -33,9 +33,14 @@ class Vendor:
 
   def _parse_product(self, product_id, data):
     #print data
-    if product_id == 1:
+    if product_id == 1 or product_id == 4:
       raw_lux = data[0] + 256*data[1]
-      luxf = raw_lux*1.24/2048
+      if product_id == 1:
+        print "raw_lux:", raw_lux
+        luxf = raw_lux*1.24/2048
+      else:
+        print "raw_lux51:", raw_lux
+        luxf = raw_lux*1.2/1024
       luxf = luxf / (10E-6*27.4E3)
       luxf = 10 **luxf
       return "lux:%d" % int(luxf)
